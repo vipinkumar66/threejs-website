42ec368ef1c62d29fa87c55585eb8620b045d77b
vipin <vipinkumarlamba55@gmail.com>
2023-04-30 11:17:36 +0530

    Initial commit/home container setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Canvas from './canvas'
+import Home from './pages/Home'
+import Customizer from './pages/Customizer'
+
+function App() {
+
+  return (
+    <main className='app transition-all ease-in'>
+     <Home/>
+     <Canvas/>
+     <Customizer/>
+    </main>
+  )
+}
+
+export default App
