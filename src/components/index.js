2146265b5771109de1fe61bfcffb0f9613ad00e2
vipin <vipinkumarlamba55@gmail.com>
2023-04-30 12:08:51 +0530

    Customizer component completed

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,14 @@
 import CustomButton from "./CustomButton";
+import AiPicker from "./CustomButton";
+import ColorPicker from "./CustomButton";
+import FilePicker from "./CustomButton";
+import Tab from "./CustomButton";
 
-export {CustomButton};
+
+export {
+    CustomButton,
+    AiPicker,
+    ColorPicker,
+    FilePicker,
+    Tab
+};
