ee38158454bf9f4d5b99688c38a46ca94e39ba8a
vipin <vipinkumarlamba55@gmail.com>
2023-04-30 13:43:35 +0530

    three js lighting and colour picker

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 import CustomButton from "./CustomButton";
-import AiPicker from "./CustomButton";
-import ColorPicker from "./CustomButton";
-import FilePicker from "./CustomButton";
-import Tab from "./CustomButton";
+import AiPicker from "./AiPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
 
 export {
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -7,46 +7,75 @@ import {download} from '../assets'
 import {downloadCanvasToImage, reader} from '../config/helpers'
 import {EditorTabs, FilterTabs, DecalTypes} from '../config/constants'
 import { fadeAnimation, slideAnimation } from '../config/motion'
-import { AiPicker, ColorPicker, FilePicker, Tab, CustomButton } from '../components'
+import { CustomButton,AiPicker,ColorPicker,FilePicker,Tab } from '../components'
 
 const Customizer = () => {
   const snap = useSnapshot(state)
+
+  const [file, setFile] = useState("")
+  const [prompt, setPrompt] = useState("")
+  const [generatingImg, setGeneratingimg] = useState("")
+
+  const [activeEditorTab, setActiveEditorTab] = useState("")
+  const [activeFilterTab, setactiveFilterTab] = useState({
+    logoshirt:true,
+    stylishShirt:false,
+  })
+
+  const generateTabContent = () =>{
+    switch(activeEditorTab){
+      case "colorpicker":
+        return <ColorPicker/>
+      case"filepicker":
+        return <FilePicker/>
+      case "aipicker":
+        return <AiPicker/>
+    }
+  }
   return (
     <AnimatePresence>
       {!snap.intro && (
         <>
-          <motion.div key="custom" className='absolute top-0 left-0 z-10' {...slideAnimation('left')}>
+          <motion.div
+           key="custom" className='absolute top-0 left-0 z-10' {...slideAnimation('left')}
+           >
             <div className='flex items-center min-h-screen'>
               <div className='editortabs-container tabs'>
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name}
-                  tab={tab}
-                  handleClick={() => {}}
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
                   />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
-          <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
+          <motion.div
+            className='absolute z-10 top-5 right-5'
+            {...fadeAnimation}
+          >
             <CustomButton
-            type="filled"
-            title="Go Back"
-            handleClick={() => state.intro = true}
-            customstyles="w-fit pz-4 py-2.5 font-bold text-sm">
-
-            </CustomButton>
-
+              type="filled"
+              title="Go Back"
+              handleClick={() => state.intro = true}
+              customstyles="w-fit pz-4 py-2.5 font-bold text-sm"
+            />
           </motion.div>
-          <motion.div className='filtertabs-container'
-          {...slideAnimation('up')}>
+          <motion.div
+            className='filtertabs-container'
+            {...slideAnimation('up')}
+          >
             {FilterTabs.map((tab) => (
-                  <Tab key={tab.name}
-                  tab={tab}
-                  isFilterTab
-                  isActiveTab =""
-                  handleClick={() => {}}
-                  />
-                ))}
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab =""
+                handleClick={() => {}}
+              />
+            ))}
           </motion.div>
         </>
       )}
